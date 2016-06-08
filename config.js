ea097217a0dc5356c687bf47c5e1053a3ac7353a
Sebastián Ramírez <sramirez10r@gmail.com>
2016-06-08 11:20:26 -0600

    Needed to be https

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   botToken: "234577996:AAHdao-7KXjmnles6RNCmK5nq1hXTBcaQ3Y",
-  prodURL: "http://telegram-pokemon.herokuapp.com/"
+  prodURL: "https://telegram-pokemon.herokuapp.com/"
 }
